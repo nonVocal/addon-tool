b2644b0a38be99318aee848c2df0dcec7a3b5073
bda <benjamin.davis@dscsag.com>
2024-06-20 10:12:00 +0200

    Fixed pom.xml  => activetable at ectr launch + FunctionProvider is found!

--- src/main/java/dev/nonvocal/omf/FunctionProvider.java
+++ src/main/java/dev/nonvocal/omf/FunctionProvider.java
@@ -1,17 +1,21 @@
 package dev.nonvocal.omf;
 
+import com.dscsag.plm.spi.interfaces.ECTRService;
 import com.dscsag.plm.spi.interfaces.gui.PluginFunction;
 import com.dscsag.plm.spi.interfaces.gui.PluginFunctionService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
-@Component(service = PluginFunctionService.class)
+@Component
 public class FunctionProvider implements PluginFunctionService
 {
+    @Reference ECTRService service;
+
     @Override
     public PluginFunction getPluginFunction(String s)
     {
         if (s.equals("fnc.edit.addons"))
-            return new OmfEditAddons();
+            return new OmfEditAddons(service);
 
         return null;
     }
--- src/main/java/dev/nonvocal/omf/OmfEditAddons.java
+++ src/main/java/dev/nonvocal/omf/OmfEditAddons.java
@@ -1,14 +1,26 @@
 package dev.nonvocal.omf;
 
+import com.dscsag.plm.spi.interfaces.ECTRService;
+import com.dscsag.plm.spi.interfaces.gui.PlmStatusLineMode;
 import com.dscsag.plm.spi.interfaces.gui.PluginFunction;
 import com.dscsag.plm.spi.interfaces.gui.PluginRequest;
 import com.dscsag.plm.spi.interfaces.gui.PluginResponse;
+import com.dscsag.plm.spi.interfaces.gui.PluginResponseFactory;
 
 public class OmfEditAddons implements PluginFunction
 {
+    ECTRService service;
+
+    public OmfEditAddons(ECTRService service)
+    {
+        this.service = service;
+    }
+
     @Override
     public PluginResponse actionPerformed(PluginRequest pluginRequest)
     {
-        return null;
+        var statusline = service.getStatusLine();
+        statusline.setText(PlmStatusLineMode.SUCCESS, "Called Addon Manager");
+        return PluginResponseFactory.infoResponse("Yeah!");
     }
 }
